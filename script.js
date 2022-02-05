dd4ae2d54d9b75d43418828589bd1600fa4860a3
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-02-04 23:35:52 -0300

    construindo verificação de cartas

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ let arrayDeGifs = [
 'assets/4.gif', 'assets/5.gif', 'assets/5.gif', 'assets/6.gif', 'assets/6.gif', 'assets/7.gif', 'assets/7.gif'
 ];
 let arrayDeCartas = [];
+let primeiraCarta;
+let segundaCarta;
+let elementoPrimeiraCarta;
+let elementoSegundaCarta;
+
+console.log(primeiraCarta, segundaCarta);
 
 while(numeroDeCartas<4 || numeroDeCartas>14 || numeroDeCartas%2 !==0){
     numeroDeCartas = parseInt(prompt("Insira o número de cartas para o jogo, de 4 a 14 cartas."));
 }
 
-// Esta função pode ficar separada do código acima, onde você preferir
+// Função para embaralhar o array de cartas do jogo
 function comparador() { 
     return Math.random() - 0.5; 
 }
@@ -23,41 +29,75 @@ function atribuirCartasNoArray(){
         arrayDeCartas[index] = arrayDeGifs[index];
     }
     return arrayDeCartas.sort(comparador);
-    
 }
 atribuirCartasNoArray();
-// console.log(arrayDeGifs, arrayDeCartas);
 
-let arrayEmbaralhado = [];
 /* 2º-> Inserir as o número de cartas válidas de forma dinâmica de acordo com o 
 HTML e após isso, embaralhar o array de cartas */
-function carregarNumeroDeCartas(arrayEmbaralhado){
+function carregarNumeroDeCartas(atribuirCartasNoArray){
     const sectionDeCartas = document.querySelector('main section');
-    // console.log(sectionDeCartas, arrayDeCartas);
     sectionDeCartas.innerHTML = '';
     
     for(let indice=0; indice<numeroDeCartas; indice++){
         sectionDeCartas.innerHTML = sectionDeCartas.innerHTML + `
-        <div class="cartas-jogo">
+        <div class="cartas-jogo" onclick="virarCarta(this)">
             <div class="frente-carta face">
                 <img src="assets/front.png" alt="Papagaios da Memória">
             </div>
-            <div class="verso-carta face">
-                <img src="${arrayEmbaralhado[indice]}" alt="Gif">
-            </div>
+            <div class="verso-carta face aparecer-verso-carta">
+                <img src="${arrayDeCartas[indice]}" alt="Gif">
+            </div>    
         </div>
         `
     }
 }
-carregarNumeroDeCartas(arrayDeCartas);
-console.log(arrayDeGifs, arrayDeCartas, arrayEmbaralhado);
+carregarNumeroDeCartas();
+console.log(arrayDeGifs, arrayDeCartas);
+
 
 /* 2.1-> Habilitar a rotação no clique da carta de acordo com os efeitos do CSS, 
-aplicatos na frente e atrás da carta */
+aplicados na frente e atrás da carta */
 
 /* 3º-> Ao clicar em uma carta, a mesma deve ser virada. Caso seja a primeira carta
 de um dos pares do array, deve permancer virada até que o usuário escolha a segunda
 carta que deseja virar */
+function virarCarta(cartaSelecionada){
+    const frenteCarta = cartaSelecionada.querySelector('.frente-carta');
+    frenteCarta.classList.add('apagar-papagaio');
+    
+    const versoCarta = cartaSelecionada.querySelector('.verso-carta');
+    versoCarta.classList.remove('aparecer-verso-carta');
+
+    if(primeiraCarta === undefined){
+        primeiraCarta = cartaSelecionada;
+        return
+    }
+        segundaCarta = cartaSelecionada;
+    
+        if(primeiraCarta.innerHTML === segundaCarta.innerHTML){
+            primeiraCarta = undefined;
+            segundaCarta = undefined;
+        }else{
+            primeiraCarta.querySelector('.verso-carta').classList.add('aparecer-verso-carta');
+            primeiraCarta.querySelector('.frente-carta').classList.remove('apagar-papagaio');
+            segundaCarta.querySelector('.frente-carta').classList.remove('apagar-papagaio');
+            segundaCarta.querySelector('.verso-carta').classList.add('aparecer-verso-carta');
+        }
+    // console.log(frenteCarta, versoCarta);
+    console.log(primeiraCarta, segundaCarta);
+}
+
+// function verificarParDeCartas(){  
+    
+//     // if(elementoPrimeiraCarta !== elementoSegundaCarta){
+//     //     primeiraCarta.querySelector('.verso-carta').classList.add('aparecer-verso-carta');
+//     //     primeiraCarta.querySelector('.frente-carta').classList.remove('apagar-papagaio');
+//     //     segundaCarta.querySelector('.verso-carta').classList.add('aparecer-verso-carta');
+//     //     segundaCarta.querySelector('.frente-carta').classList.remove('apagar-papagaio');
+//     //     primeiraCarta = undefined;
+//     //     segundaCarta = undefined;
+//     // }
+// }
 
 /* 4º-> Com a segunda carta virada, existem duas situações:
     - Caso seja igual à primeira carta, o usuário acertou e ambas agora devem ficar 
